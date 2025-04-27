8ac0cb533e704ab39fe930b76583224703d65952
Gyeongyeon Jeon <gjeon0321@gmail.com>
2025-04-28 04:08:30 +0900

    feat: editor design

--- src/app/(dashboard)/documents/[id]/page.tsx
+++ src/app/(dashboard)/documents/[id]/page.tsx
@@ -1,27 +1,37 @@
+import { CgFileDocument } from "react-icons/cg";
+import { FaRegComment } from "react-icons/fa";
+import { FaShare } from "react-icons/fa6";
+import { HiPlus } from "react-icons/hi";
+import { IoIosArrowDown, IoMdMenu } from "react-icons/io";
+import { LuHistory } from "react-icons/lu";
+
 export default function DocumentEditPage() {
   return (
-    <div className="h-full">
+    <div className="h-full flex flex-col divide-y divide-border-primary text-foreground">
       {/* HEADER */}
-      <div className="flex justify-between">
-        <div className="flex items-center gap-2">
-          <div>menu</div>
-          <div className="flex items-center gap-2">
-            <h2>Untitled Document</h2>
-            <div>arrow icon</div>
+      <div className="flex justify-between bg-background h-14 px-5">
+        <div className="flex items-center gap-4">
+          <IoMdMenu size={20} />
+          <div className="flex items-center gap-2 w-44 justify-between">
+            <h2 className="text-dark-gray">Untitled Document</h2>
+            <IoIosArrowDown size={18} />
           </div>
         </div>
-        <div className="flex items-center gap-2">
-          <div>saved</div>
+        <div className="flex items-center gap-4">
+          <div className="text-dark-gray">Saved</div>
           <div>share icon</div>
-          <div>Share Btn</div>
-          <div>comment</div>
-          <div>history</div>
+          <div className="flex items-center bg-button-primary-background text-button-primary-text py-1 px-3 rounded-lg">
+            <FaShare />
+            Share
+          </div>
+          <FaRegComment />
+          <LuHistory />
         </div>
       </div>
       {/* CONTENT */}
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col h-full bg-background divide-y divide-border-primary">
         {/* TOOLBAL */}
-        <div className="flex gap-2">
+        <div className="flex gap-2 h-10 items-center">
           <div>undo</div>
           <div>redo</div>
           <div>bold</div>
@@ -34,35 +44,60 @@ export default function DocumentEditPage() {
           <div>image</div>
         </div>
         {/* EDITOR */}
-        <div className="flex flex-1">
+        <div className="flex flex-1 divide-x divide-border-primary">
           {/* LIST */}
-          <div className="flex gap-2 flex-col bg-[#F9FAFB] w-1/4 h-full">
-            <div className="flex gap-2">
+          <div className="flex gap-2 flex-col w-60 h-full px-3">
+            <div className="flex gap-2 items-center w-full h-12 justify-between font-bold">
               <div>Documents</div>
-              <div>Plus</div>
+              <HiPlus />
             </div>
-            <div className="flex flex-col">
-              <div>Current Document</div>
-              <div>Document 2</div>
-              <div>Document 3</div>
-              <div>Document 4</div>
-              <div>Document 5</div>
-            </div>
+            <ul className="flex flex-col gap-1">
+              <li className="flex gap-2 items-center h-8 bg-light-gray px-2 rounded-lg">
+                <CgFileDocument />
+                Current Document
+              </li>
+              <li className="flex gap-2 items-center h-8 px-2">
+                <CgFileDocument />
+                Document 2
+              </li>
+              <li className="flex gap-2 items-center h-8 px-2">
+                <CgFileDocument />
+                Document 3
+              </li>
+              <li className="flex gap-2 items-center h-8 px-2">
+                <CgFileDocument />
+                Document 4
+              </li>
+              <li className="flex gap-2 items-center h-8 px-2">
+                <CgFileDocument />
+                Document 5
+              </li>
+            </ul>
           </div>
           {/* EDITOR */}
-          <div className="flex flex-col flex-1 bg-red-50" />
+          <div className="flex flex-col flex-1 bg-background-editor" />
           {/* COMMENTS */}
-          <div className="flex flex-col bg-[#F9FAFB] w-1/4 h-full">
-            <div className="flex gap-2">
+          <div className="flex gap-2 flex-col w-60 h-full px-3">
+            <div className="flex gap-2 items-center w-full h-12 justify-between font-bold">
               <div>Comments</div>
-              <div>Plus</div>
+              <HiPlus />
             </div>
-            <div className="flex flex-col">
-              <div>Comment 1</div>
-              <div>Comment 2</div>
-              <div>Comment 3</div>
-              <div>Comment 4</div>
-              <div>Comment 5</div>
+            <ul className="flex flex-col">
+              <li>Comment 1</li>
+              <li>Comment 2</li>
+              <li>Comment 3</li>
+              <li>Comment 4</li>
+              <li>Comment 5</li>
+            </ul>
+            <div>
+              <div className="flex gap-2 items-center w-full h-12 justify-between font-bold">
+                Active Now
+              </div>
+              <ul className="flex flex-col text-sm">
+                <li>You</li>
+                <li>권규</li>
+                <li>이린</li>
+              </ul>
             </div>
           </div>
         </div>
